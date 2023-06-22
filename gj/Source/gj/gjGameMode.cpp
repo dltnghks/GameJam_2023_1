6ac8e7931eca88cb6e9b50ae8543a693f951e1fc
dltnghks <55356632+dltnghks@users.noreply.github.com>
2023-06-22 21:06:37 +0900

    에셋 추가, 장애물 배치

--- gj/Source/gj/gjGameMode.cpp
+++ gj/Source/gj/gjGameMode.cpp
@@ -6,10 +6,4 @@
 
 AgjGameMode::AgjGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
